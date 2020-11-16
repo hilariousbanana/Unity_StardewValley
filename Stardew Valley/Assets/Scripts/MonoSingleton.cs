7179f02db7f08f95dea07f93b9010fe8049986a9
kwen won lee <highfence@naver.com>
2020-11-17 02:15:50 +0900

    Implement simple mono behaviour singleton class

---
+++ Stardew Valley/Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static bool isShuttingDown = false;
+    private static object lockObject = new object();
+    private static T innerInstance;
+
+    public static T instance
+    {
+        get
+        {
+            if (isShuttingDown)
+            {
+                Debug.LogWarning($"[Singleton] Instance '{nameof(T)}' already destroyed. Returning null.");
+                return null;
+            }
+
+            lock (lockObject)
+            {
+                if (innerInstance)
+                {
+                    return innerInstance;
+                }
+
+                innerInstance = FindObjectOfType(typeof(T)) as T;
+                if (innerInstance)
+                {
+                    return innerInstance;
+                }
+                
+                innerInstance = new GameObject().AddComponent<T>();
+                innerInstance.gameObject.name = $"{nameof(T)} + (Singleton)";
+                
+                DontDestroyOnLoad(innerInstance);
+
+                return innerInstance;
+            }
+        }
+    }
+
+    private void OnApplicationQuit() => isShuttingDown = true;
+
+    private void OnDestroy() => isShuttingDown = true;
+}
